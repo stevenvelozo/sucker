aa181c83d644e5bd71a455494345f11931ebcf58
Steven Velozo <steven@Stevens-MacBook-Pro.local>
2018-02-21 08:44:41 -0800

    Adding entity cache

--- source/Components/EntityCache.js
+++ source/Components/EntityCache.js
@@ -13,6 +13,45 @@ class EntityCache
 	{
 		this.fable = pFable;
 		this.webserver = pWebServer;
+
+		this.cacheData = {};
+	}
+
+	// Add an entity record 
+	add(pEntity, pGUID, pRecord)
+	{
+		if (typeof(this.cacheData[pEntity]) !== 'object')
+			this.cacheData[pEntity] = {};
+
+		this.cacheData[pEntity][pGUID] = pRecord;
+	}
+
+	read(pEntity, pGUID)
+	{
+		if (typeof(this.cacheData[pEntity]) !== 'object')
+			return false;
+
+		if (typeof(this.cacheData[pEntity][pGUID]) !== 'object')
+			return false;
+
+		return this.cacheData[pEntity][pGUID];
+	}
+
+	clear()
+	{
+		this.cacheData = {};
+
+		return true;
+	}
+
+	clearEntity(pEntity)
+	{
+		if (typeof(this.cacheData[pEntity]) !== 'object')
+			return false;
+
+		this.cacheData[pEntity] = {};
+
+		return true;
 	}
 }
 
